2f9e384636be9096f06ec72adcdcc65e6801e95d
Lucas Leifheit Nardon <43593024+lnardon@users.noreply.github.com>
2023-10-17 23:42:44 +0000

    Update main package to user local scaler

--- main.go
+++ main.go
@@ -2,36 +2,43 @@ package main
 
 import (
 	"fmt"
-	"image"
 	"os"
+	"strconv"
 	"strings"
-
-	"golang.org/x/image/draw"
 
 	//Local imports
 	"github.com/lnardon/VanGogh/decoder"
 	"github.com/lnardon/VanGogh/encoder"
+	"github.com/lnardon/VanGogh/scaler"
 )
 
 func main() {
 	fmt.Println("VanGogh - An image manipulation tool written in Go.")
-    args := os.Args
+	args := os.Args
 
-    input, _ := os.Open(args[1])
-    defer input.Close()
-    outputFile, _ := os.Create("image_resized.png")
-    defer outputFile.Close()
-    extension := input.Name()[strings.LastIndex(input.Name(),"."):]
-    src,err := decoder.DecodeImage(input,extension)
-    
-    if err != nil {
-      fmt.Println(err)
-      } else {
-      fmt.Println("Resizing...")
-      blankImage := image.NewRGBA(image.Rect(0, 0, src.Bounds().Max.X/10, src.Bounds().Max.Y/10))
-      draw.NearestNeighbor.Scale(blankImage, blankImage.Rect, src, src.Bounds(), draw.Over, nil)
-      encoder.EncodeImage(outputFile,blankImage, extension)
-      fmt.Println("Done.")
-    }
+	input, _ := os.Open(args[1])
+	scalingTechnique := args[2]
+	scalingFactor, _ := strconv.Atoi(args[3])
+	
+	defer input.Close()
+	outputFile, _ := os.Create("image_resized.png")
+	defer outputFile.Close()
+	extension := input.Name()[strings.LastIndex(input.Name(), "."):]
+	src, err := decoder.DecodeImage(input, extension)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	info, err := os.Stat(args[1])
+	isBathOperation := err == nil && info.IsDir()
+	if isBathOperation {
+		// TODO: HANDLE BATCH
+		fmt.Println("FOI")
+	} else {
+		fmt.Println("Resizing...")
+		newImage := scaler.ScaleImage(src, scalingTechnique, scalingFactor)
+		encoder.EncodeImage(outputFile, newImage, extension)
+		fmt.Println("Done.")
+	}
 }
-
