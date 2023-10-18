690dd58d370e906178157d371f0cfc05aa96bd91
lnardon <lucaslnardon@gmail.com>
2023-10-18 03:33:58 -0300

    ♻️ Code refactor

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"strconv"
 	"strings"
 
-	//Local imports
 	"github.com/lnardon/VanGogh/decoder"
 	"github.com/lnardon/VanGogh/encoder"
 	"github.com/lnardon/VanGogh/scaler"
@@ -14,31 +13,32 @@ import (
 
 func main() {
 	fmt.Println("VanGogh - An image manipulation tool written in Go.")
-	args := os.Args
-
-	input, _ := os.Open(args[1])
-	scalingTechnique := args[2]
-	scalingFactor, _ := strconv.Atoi(args[3])
 	
-	defer input.Close()
-	outputFile, _ := os.Create("image_resized.png")
-	defer outputFile.Close()
-	extension := input.Name()[strings.LastIndex(input.Name(), "."):]
-	src, err := decoder.DecodeImage(input, extension)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	info, err := os.Stat(args[1])
-	isBathOperation := err == nil && info.IsDir()
-	if isBathOperation {
-		// TODO: HANDLE BATCH
-		fmt.Println("FOI")
+	info, err := os.Stat(os.Args[1])
+	isBatchOperation := err == nil && info.IsDir()
+	if isBatchOperation {
+		// TODO: handle batch and make it work with goroutines
+		fmt.Println("Unable to handle batch operations yet.")
 	} else {
-		fmt.Println("Resizing...")
-		newImage := scaler.ScaleImage(src, scalingTechnique, scalingFactor)
-		encoder.EncodeImage(outputFile, newImage, extension)
-		fmt.Println("Done.")
+	  args := os.Args
+      input, _ := os.Open(args[1])
+      scalingTechnique := args[2]
+      scalingFactor, _ := strconv.Atoi(args[3])
+      fileName := input.Name()[strings.LastIndex(input.Name(), "/")+1:]
+      extension := fileName[strings.LastIndex(fileName, "."):]
+      
+      defer input.Close()
+      outputFile, _ := os.Create("output/" + fileName)
+      defer outputFile.Close()
+      
+	  src, err := decoder.DecodeImage(input, extension)
+      if err != nil {
+        fmt.Println(err,"Error decoding image.")
+      }
+
+      fmt.Println("Resizing...")
+      newImage := scaler.ScaleImage(src, scalingTechnique, scalingFactor)
+      encoder.EncodeImage(outputFile, newImage, extension)
+      fmt.Println("Done.")
 	}
 }
